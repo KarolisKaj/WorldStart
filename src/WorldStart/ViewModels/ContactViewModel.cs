d1b232a55c7a810ccf66f576f1a0d89eeb88d18d
Karolis Kajėnas <karolis.kajenas1@gmail.com>
2017-02-16 19:40:16 +0200

    Added menu item links. Moving towards MVC.

---
+++ src/WorldStart/ViewModels/ContactViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WorldStart.ViewModels
+{
+    public class ContactViewModel
+    {
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(4069, MinimumLength = 6)]
+        public string Message { get; set; }
+    }
+}
