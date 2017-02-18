5d0dc073053d88baa772ce33b207d10b55ad865b
Karolis Kajėnas <karolis.kajenas1@gmail.com>
2017-02-18 21:49:59 +0200

    Added mail service, di, environment checkings, vm validations.

--- src/WorldStart/Controllers/Web/AppController.cs
+++ src/WorldStart/Controllers/Web/AppController.cs
@@ -1,8 +1,21 @@
 ﻿namespace WorldStart.Controllers.Web
 {
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Configuration;
+    using Services;
+    using ViewModels;
+
     public class AppController : Controller
     {
+        private readonly IMailService _mailService;
+        private readonly IConfigurationRoot _configuration;
+
+        public AppController(IMailService mailService, IConfigurationRoot configuration)
+        {
+            _mailService = mailService;
+            _configuration = configuration;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -10,6 +23,22 @@
 
         public IActionResult About()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (model.Email.Contains("aol.com"))
+                ModelState.AddModelError("", "Nop, not good. Cya"); // Adds general errors to model, first param specific prop.
+
+            if (ModelState.IsValid)
+            {
+                _mailService.SendMail(_configuration["MailSettings:ToAddress"], model.Email, model.Name, model.Message);
+
+                ViewBag.UserMessage = "Message Sent";
+                ModelState.Clear();
+            }
             return View();
         }
 
---
+++ src/WorldStart/Services/DebugMailService.cs
@@ -0,0 +1,13 @@
+﻿namespace WorldStart.Services
+{
+    using System;
+    using System.Diagnostics;
+
+    public class DebugMailService : IMailService
+    {
+        public void SendMail(string to, string from, string subject, string body)
+        {
+            Debug.WriteLine($"{to}, {from}, {subject}");
+        }
+    }
+}
